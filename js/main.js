0c5b75d8131911cf56eec89d0fc57acfe1f42602
Michele <micheleierniero@gmail.com>
2021-07-21 13:18:29 +0200

    snack 1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// snack 1
+
+var yourNumber = parseInt(prompt("Inserisci un numero:"));
+
+if (yourNumber % 2 == 0) {
+    console.log(yourNumber);
+}
+
+else {
+    console.log(yourNumber + 1);
+}
